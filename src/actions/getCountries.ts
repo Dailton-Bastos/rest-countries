8efdce6b0f02b23037f4cf8cccf4ac9a5e765926
Dailton Bastos <dailtonbastos@gmal.com>
2023-12-17 00:27:23 -0300

    feat(getcountries): add filter fields

--- src/actions/getCountries.ts
+++ src/actions/getCountries.ts
@@ -4,7 +4,9 @@ import type { Country } from '@/@types/country'
 
 export const getCountries = async (): Promise<Country[]> => {
   try {
-    const res = await fetch('https://restcountries.com/v3.1/all')
+    const res = await fetch(
+      'https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags'
+    )
 
     if (!res.ok) {
       return []
